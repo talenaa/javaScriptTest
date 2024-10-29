fe96ca552722163c838af51a2165c3b891cbdc1e
talenaa <talenabarbosa7@gmail.com>
2024-10-29 11:28:46 +0100

    just testing javascript for a project

---
+++ js/index.js
@@ -0,0 +1,43 @@
+
+let number = 5;
+let numberFloat = 3.739572;
+let myName = "talena";
+let resultBoolean = true; 
+let nameList2 = ["Talena", 18, "Valeria", 20, "Rebeca", 19];
+let nameList2D = [["Talena", "Brazil"], ["Valeria", "Spain"], ["Rebeca", "Spain"]];
+console.log(nameList2D[0][1]);
+
+let nameListCV = [
+    {name: "Talena", country: "Brazil", age: 18},
+    {name: "Valeria", country: "Spain", age: 20},
+    {name: "Rebeca", country: "Spain", age: 19}
+];
+
+const numberPi = 3.141592;
+const maxStudent = 25;
+let numberStudent = 17;
+
+//calculadora
+
+function sumTwoNumbers(firstNumber, secondNumber){
+    let result
+    result = firstNumber + secondNumber;
+    return (result);
+}
+
+function subTwoNumbers(firstNumber, secondNumber){
+    let result
+    result = firstNumber + secondNumber;
+    return (result);
+}
+
+
+let number1 = 5;
+let number2 = 7;
+sumResult = sumTwoNumbers(number1, number2);
+console.log(sumResult);
+subResult = subTwoNumbers(number1, number2);
+console.log(subTwoNumbers);
+
+
+
